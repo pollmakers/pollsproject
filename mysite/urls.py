15f505cbc4f402d63415f8e972ffcb4651812964
amo <kwamena.amo-dadey@amalitech.org>
2021-06-04 11:54:21 +0000

    added config for favicons

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from polls.views import profile
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls',namespace = 'polls')),
     path('accounts/', include('allauth.urls')),
-    path('accounts/profile/',profile)
+    path('accounts/profile/',profile),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    )
 ]
